b0ace9883347636f1879ccfb8ef0306d061850a3
BZednenem <bautimenendez77@gmail.com>
2025-05-29 14:48:57 -0300

    Add files via upload

---
+++ 9_Alvarez_M/0_PC9_7/0_PC9_7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_PC9_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1;
+            int numero2;
+            int resultado;
+            Console.WriteLine("Ingrese el primer número:");
+            numero1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo número:");
+            numero2 = int.Parse(Console.ReadLine());
+
+            resultado = numero1 / numero2;
+            Console.WriteLine("El resultado es: " + resultado);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 9_Alvarez_M/0_PC9_9/0_PC9_9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_PC9_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double volumen;
+            double superficie;
+            double radio;
+            Console.WriteLine("Para calcular el volumen y la superficie de una esfera, primero hay que conocer su radio. Ingreselo:");
+            radio = int.Parse(Console.ReadLine());
+
+            superficie = 4 * 3.14 * (Math.Pow(radio, 2));
+            volumen = 4 * 3.14 * (Math.Pow(radio, 3)) / 3;
+            Console.WriteLine("La superficie es: " + superficie);
+            Console.WriteLine("El volumen es: " + volumen);
+
+            Console.ReadKey();
+        }
+    }
+}
