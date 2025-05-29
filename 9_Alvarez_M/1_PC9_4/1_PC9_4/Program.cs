503af5df7f17ac66c69aaf552aa5e3aef3c7a103
BZednenem <bautimenendez77@gmail.com>
2025-05-29 15:00:50 -0300

    Add files via upload

---
+++ 9_Alvarez_M/1_PC9_4/1_PC9_4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_PC9_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese su edad: ");
+            int edad = int.Parse(Console.ReadLine());
+
+            Console.Write("Indique sus ingresos mensuales: ");
+            int ing = int.Parse(Console.ReadLine());
+
+            if ((edad > 19) && (ing <= 100000))
+            {
+                Console.WriteLine("Usted puede cobrar la beca.");
+            }
+
+            if ((edad < 19) && (ing == 0))
+            {
+                Console.WriteLine("Usted puede cobrar la beca.");
+            }
+
+            if ((edad == 19) && (ing <= 50000))
+            {
+                Console.WriteLine("Usted puede cobrar la beca.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 9_Alvarez_M/1_PC9_5/1_PC9_5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_PC9_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("¿Tenes comida? (sí o no):");
+                string comida = Console.ReadLine().ToLower();
+
+                Console.WriteLine("¿Tenes refugio? (sí o no):");
+                string refugio = Console.ReadLine().ToLower();
+
+                Console.WriteLine("¿Tenes un bate? (sí o no):");
+                string bate = Console.ReadLine().ToLower();
+
+                if (comida == "sí" && refugio == "sí" && bate == "sí")
+                {
+                    Console.WriteLine("¡Sobrevives!");
+                }
+                else
+                {
+                    Console.WriteLine("No sobrevives.");
+                }
+                Console.ReadKey();
+            }
+        }
+
+
+    }
+}
+
