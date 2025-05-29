bb3706144df46264b7c30f046f73bbf3534a7243
BZednenem <bautimenendez77@gmail.com>
2025-05-29 14:43:26 -0300

    Add files via upload

---
+++ 9_Alvarez_M/0_PC9_3/0_PC9_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_PC9_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("¿Cuál es tu nombre?");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine("¿Cuantos años tenes?");
+            
+            string edad = Console.ReadLine();
+
+            Console.WriteLine("Hola " + nombre + ", tu edad es: " + edad);
+            Console.ReadKey();
+        }
+    }
+}
