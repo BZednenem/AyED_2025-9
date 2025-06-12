29d3a8b198bbcd82dfac4cffc55b0981ebabfad9
BZednenem <bautimenendez77@gmail.com>
2025-06-12 15:01:07 -0300

    Add files via upload

---
+++ 9_Menendez_J/0_PC9_1/0_PC9_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_PC9_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string holamundo = "Hola Mundo!";
+
+            Console.WriteLine(holamundo);
+
+            Console.ReadKey();
+        }
+    }
+}
