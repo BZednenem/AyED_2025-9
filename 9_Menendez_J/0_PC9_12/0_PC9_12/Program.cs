19860507d3baa735ee6e648e27fe84b384400f3f
BZednenem <bautimenendez77@gmail.com>
2025-06-12 15:14:25 -0300

    Add files via upload

---
+++ 9_Menendez_J/0_PC9_12/0_PC9_12/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_PC9_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string palabra;
+            Console.Write("Escriba la palabra: ");
+            palabra = Console.ReadLine();
+
+            char[] caracteres = palabra.ToCharArray();
+            Array.Reverse(caracteres);
+
+            string cadenaInvertida = new string(caracteres);
+
+            Console.Write(cadenaInvertida);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 9_Menendez_J/1_PC9_0/1_PC9_0/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_PC9_0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad;
+
+            Console.Write("Indique cual es su edad: ");
+            edad = int.Parse(Console.ReadLine());
+
+            if (edad >= 18)
+            {
+                Console.WriteLine("Usted es mayor de edad.");
+            }
+            else
+            {
+                Console.WriteLine("Usted es menor de edad.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
