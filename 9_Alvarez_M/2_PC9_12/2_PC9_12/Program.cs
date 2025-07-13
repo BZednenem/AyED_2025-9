578a567128970d51e9e90fd18c4684f5799bea2c
STazeravla <santinoalvarez325@gmail.com>
2025-07-13 12:29:55 -0300

    Add files via upload

---
+++ 9_Alvarez_M/2_PC9_12/2_PC9_12/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+int n;
+Random random = new Random();
+random.Next(1, 101); // Genera un número aleatorio entre 1 y 100
+
+
+Console.WriteLine("Ingrese el número de filas y columnas (n):");
+n = int.Parse(Console.ReadLine());
+
+int[,] matriz1 = new int[n, n];
+
+Console.WriteLine("Matriz1:");
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        matriz1[i, j] = random.Next(1, 101);
+        Console.Write(matriz1[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+
+int[] diagonal = new int[n];
+
+Console.WriteLine("Diagonal principal de la matriz:");
+for (int i = 0; i < n; i++)
+{
+    diagonal[i] = matriz1[i, i];
+    Console.Write(diagonal[i] + "\t");
+}
+Console.WriteLine();
+
+Console.ReadKey();
---
+++ 9_Alvarez_M/2_PC9_13/2_PC9_13/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Ingrese la cantidad de estudiantes: ");
+int n = int.Parse(Console.ReadLine());
+
+string[,] estudiantes = new string[n, 3];
+
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine($"\nEstudiante {i + 1}:");
+
+    Console.Write("Nombre: ");
+    estudiantes[i, 0] = Console.ReadLine();
+
+    Console.Write("Edad: ");
+    estudiantes[i, 1] = Console.ReadLine();
+
+    Console.Write("Calificación: ");
+    estudiantes[i, 2] = Console.ReadLine();
+}
+
+Console.WriteLine("\nListado de estudiantes:");
+Console.WriteLine("N°\tNombre\tEdad\tCalificación");
+
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine($"{i + 1}\t{estudiantes[i, 0]}\t{estudiantes[i, 1]}\t{estudiantes[i, 2]}");
+}
+
