2ad0618e47df81509ed04464f38b193c2148e063
STazeravla <santinoalvarez325@gmail.com>
2025-07-13 12:00:36 -0300

    Add files via upload

---
+++ 9_Alvarez_M/1_PC9_14/1_PC9_14/1_PC9_14/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_PC9_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion;
+            string nombre = "Jugador1";
+            int salud = 100;
+            int monedas = 50;
+            int enemigosDerrotados = 0;
+            bool tieneEspada = false;
+
+            bool continuar = true;
+            while (continuar)
+            {
+                Console.WriteLine("---MENÚ---");
+                Console.WriteLine("Opción 1: Mostrar estado del jugador.");
+                Console.WriteLine("Opción 2: Encontrar espada mágica.");
+                Console.WriteLine("Opción 3: Pelear contra un enemigo.");
+                Console.WriteLine("Opción 4: Comprar poción de vida (vale 20 monedas).");
+                Console.WriteLine("Opción 5: Salir del juego.");
+                Console.WriteLine("Seleccione una opción.");
+
+
+                if (int.TryParse(Console.ReadLine(), out opcion))
+                {
+                    switch (opcion)
+                    {
+                        case 1:
+                            Console.WriteLine("Nombre: "+nombre);
+                            Console.WriteLine("Salud: "+salud);
+                            Console.WriteLine("Monedas: "+monedas);
+                            Console.WriteLine("Enemigos derrotados: "+enemigosDerrotados);
+                            Console.WriteLine("Tiene espada: "+tieneEspada); 
+                            break;
+
+                        case 2:
+                            if (tieneEspada == false)
+                            {
+                                tieneEspada = true;
+                                Console.WriteLine("¡Encontraste la espada mágica!.");
+                            }
+                            else if (tieneEspada == true)
+                            {
+                                Console.WriteLine("Ya tenés la espada mágica.");
+                            }
+                            break;
+
+                        case 3:
+                            if (tieneEspada == true)
+                            {
+                                salud = salud - 10;
+                                enemigosDerrotados = enemigosDerrotados + 1;
+                                Console.WriteLine("¡Ganaste la batalla! Salud -10.");
+                            }
+                            else if (tieneEspada == false)
+                            {
+                                salud = salud - 30;
+                                
+                                Console.WriteLine("¡Fue una pelea dura sin espada! Salud -30.");
+                            }
+                            if (salud <= 0)
+                            {
+                                continuar = false;
+                                Console.WriteLine("Moriste.");
+                            }
+                            break;
+
+                        case 4:
+                            if (monedas >= 20)
+                            {
+                                monedas = monedas - 20;
+                                salud = salud + 20;
+                                Console.WriteLine("Usaste una poción. +20 de salud.");
+                            }
+                            else if (monedas < 20)
+                            {
+                                Console.WriteLine("No tenés suficientes monedas.");
+                            }
+                            break;
+
+                        case 5:
+                            continuar = false;
+                            Console.WriteLine("¡Gracias por jugar!");
+                            break;
+                    }
+
+
+
+                }
+                else
+                {
+                    Console.WriteLine("Opción inválida. Elegí una opción del 1 al 5.");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 9_Alvarez_M/1_PC9_15/1_PC9_15/1_PC9_15/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_PC9_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion;
+            int salud = 100;
+            int hambre = 50;
+            int energia = 70;
+            int dia = 1;
+            bool sigueVivo = true;
+            Random rand = new Random();
+            int probabilidad = rand.Next(1, 101);
+
+            while (sigueVivo)
+            {
+                Console.WriteLine("Día " + dia + " – ¿Qué querés hacer?");
+                Console.WriteLine("1. Buscar comida.");
+                Console.WriteLine("2. Dormir.");
+                Console.WriteLine("3. Explorar la isla.");
+                Console.WriteLine("4. Ver estado del personaje.");
+                Console.WriteLine("5. Salir del juego.");
+
+                if (int.TryParse(Console.ReadLine(), out opcion))
+                {
+                    switch (opcion)
+                    {
+                        case 1:
+                            hambre = hambre + 20;
+                            energia = energia - 15;
+
+                            if (probabilidad <= 30)
+                            {
+                                salud = salud - 15;
+                                Console.WriteLine("Comiste algo en mal estado.Salud - 15.");
+                            }
+
+                            if (salud <= 0 || hambre <= 0 || energia <= 0)
+                            {
+                                Console.WriteLine("Te desmayaste y no pudiste sobrevivir... Game Over☠️");
+                                sigueVivo = false;
+                            }
+                            dia = dia + 1;
+                            break;
+
+                        case 2:
+                            energia = energia + 30;
+                            hambre = hambre - 10;
+                            Console.WriteLine("Dormiste.");
+                            if (salud <= 0 || hambre <= 0 || energia <= 0)
+                            {
+                                Console.WriteLine("Te desmayaste y no pudiste sobrevivir... Game Over");
+                                sigueVivo = false;
+                            }
+                            dia = dia + 1;
+                            break;
+
+                        case 3:
+                            energia = energia - 20;
+                            hambre = hambre - 15;
+
+                            if (probabilidad <= 50)
+                            {
+                                salud = salud + 10;
+                                Console.WriteLine("Comiste algo en mal estado.Salud - 15.");
+                            }
+
+                            if (salud <= 0 || hambre <= 0 || energia <= 0)
+                            {
+                                Console.WriteLine("Te desmayaste y no pudiste sobrevivir... Game Over☠️");
+                                sigueVivo = false;
+                            }
+                            dia = dia + 1;
+                            break;
+
+                        case 4:
+                            Console.WriteLine("Salud: " + salud);
+                            Console.WriteLine("Hambre: " + hambre);
+                            Console.WriteLine("Energía: " + energia);
+                            dia = dia + 1;
+                            break;
+
+                        case 5:
+                            sigueVivo = false;
+                            Console.WriteLine("Adiós.");
+                            break;
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
