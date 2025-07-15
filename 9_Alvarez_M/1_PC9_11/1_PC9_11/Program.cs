3ad5164745d523b23a7251e69e2c5bddafd6fc1c
STazeravla <santinoalvarez325@gmail.com>
2025-07-14 22:21:31 -0300

    Add files via upload

---
+++ 9_Alvarez_M/1_PC9_11/1_PC9_11/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Ingrese un número N: ");
+if (int.TryParse(Console.ReadLine(), out int N))
+{
+    Console.WriteLine("Números pares del 1 al " + N + ":");
+    for (int i = 1; i <= N; i++)
+    {
+        if (i % 2 == 0)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
+Console.ReadKey();
